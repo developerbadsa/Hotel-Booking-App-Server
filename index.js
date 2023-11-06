9d8fcae64761606b0b9d5822c6142af6704aaf06
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-11-06 23:14:02 +0600

    made get req for res response of Booking from server by depend email of user via query

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require("cors")
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
@@ -16,25 +16,20 @@ const client = new MongoClient(uri, {
   },
 });
 
-// middlewares 
+// middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
 async function run() {
-
-      const RoomsDB = client.db("RoomsDB").collection("roomCollection")
-
-
-
+  const RoomsDB = client.db("RoomsDB").collection("roomCollection");
 
   try {
-
-
-
     //all get api===========================================
     app.get("/rooms", async (req, res) => {
       try {
-        const result = await RoomsDB.find({ Availability: 'available' }).toArray();
+        const result = await RoomsDB.find({
+          Availability: "available",
+        }).toArray();
         res.send(result);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -42,52 +37,50 @@ async function run() {
       }
     });
 
-    
     app.get("/room_details/:RoomTitle", async (req, res) => {
-      const RoomTitle = req.params
+      const RoomTitle = req.params;
       try {
-        const result = await RoomsDB.findOne(RoomTitle)
-      res.send(result)
+        const result = await RoomsDB.findOne(RoomTitle);
+        res.send(result);
       } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).send("Internal Server Error");
       }
-    })
+    });
 
+    app.get("/my_bookings", async (req, res) => {
+      const userEmail = req.query.email.replace(/'/g, "");
+      if (!userEmail) {
+        return res.status(400).json({ error: "User email not provided" });
+      }
+      const UserDatasDB = client.db("UserDatas").collection(userEmail);
+      try {
+        const result = await UserDatasDB.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
-
-//     post api ===============================
-app.post('/room_details', async (req, res)=>{
-      const userEmail = req.query.email
-      const cardData = req.body
+    //     post api ===============================
+    app.post("/room_details", async (req, res) => {
+      const userEmail = req.query.email;
+      const cardData = req.body;
 
       if (!userEmail) {
-            return res.status(400).json({ error: 'User email not provided' });
-          }
+        console.log("not found");
+        return res.status(400).json({ error: "User email not provided" });
+      }
 
-          try{
-            const UserDatasDB = client.db('UserDatas').collection(userEmail)
+      try {
+        const UserDatasDB = client.db("UserDatas").collection(userEmail);
 
-            const result = await UserDatasDB.insertOne({cardData});
-            res.send(result)
-          }
-          catch(err){
-            console.log(err)
-          }
-})
-   
-
-
-
-
-
-
-
-
-
-
-
-
+        const result = await UserDatasDB.insertOne(cardData);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -99,8 +92,8 @@ app.post('/room_details', async (req, res)=>{
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-      res.send("Running server");
-    });
+  res.send("Running server");
+});
 
 // Start the server
 app.listen(port, () => {
