81fc14997a92f9f9066ef309cea754436c682817
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-11-06 07:19:50 +0600

    created get api for send res for rooms page

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.get('/',(req, res)=>{
-      res.send('bismillah')
-})
+const uri = process.env.MONGODB_URI;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// middlewares 
+app.use(cors());
+
+async function run() {
+
+      const RoomsDB = client.db("RoomsDB").collection("roomCollection")
+
+
+
+
+  try {
+
+
+
+    //all get api
+    app.get("/rooms", async (req, res) => {
+      try {
+        const result = await RoomsDB.find({ Availability: 'available' }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+      res.send("Running server");
+    });
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
