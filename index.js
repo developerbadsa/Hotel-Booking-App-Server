0ab0d7bada61dd9316594dfd7e8b4163ac97611d
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-11-07 16:09:03 +0600

    added jwt route for set cookie as jwt token to client

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
@@ -14,12 +14,17 @@ const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
+    deprecationErrors: false,
   },
 });
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -88,6 +93,26 @@ async function run() {
         console.log(err);
       }
     });
+
+    //     jwt
+    app.post("/jwt", (req, res) => {
+      const userEmail = req.query.email;
+
+      //create toten in server
+      const token = jwt.sign({ userEmail }, process.env.SECRET_KEY, {
+        expiresIn: "1h",
+      });
+
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 100000),
+        httpOnly: false,
+        secure: true,
+        sameSite: "none",
+      });
+      res.send("data");
+      console.log(token);
+    });
+
 
     //update requests
 
